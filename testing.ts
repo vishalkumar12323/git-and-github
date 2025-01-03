ee96cf79ee3c02044c1de14a307ac9fd6b0183e1
Vishal Kumar <vishalninaniya8949info@gmail.com>
2025-01-03 13:54:05 +0530

    update testing.js

--- testing.ts
+++ testing.ts
@@ -2,8 +2,16 @@ type typeNumber = {
   (num: number): void;
 };
 
+type DomEvent = {
+  target: HTMLElement;
+  type: string;
+};
 function run(): typeNumber {
   return (num: number) => {
     console.log(num);
   };
+}
+
+function handleEvent(event: DomEvent) {
+  console.log(event.type);
 }
